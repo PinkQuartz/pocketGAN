efe835f83b80709f0bbc06096ee46d935b411f0f
Aidan Moran <aimoran@protonmail.com>
2022-06-27 19:03:19 -0500

    Fixed Balck bars on image sides
    Scale will now scale based on the smallest size, removing the black bars on the sides of tall images

--- IMAGE2TRAINING.py
+++ IMAGE2TRAINING.py
@@ -14,17 +14,29 @@ except:
 
 for each in tqdm(os.listdir(src)):
     ## Resize image to have a height of baseHeight
-    baseHeight = 256
+    base = 256
     png = Image.open(os.path.join(src,each))
-    hpercent = (baseHeight / float(png.size[1]))
-    wsize = int((float(png.size[0]) * float(hpercent)))
-    png = png.resize((wsize, baseHeight), Image.Resampling.LANCZOS)
+    width, height = png.size
+
+    ## Basing scale off smallest side stops black bars from appearing on the images
+    if height <= width:
+        ##Resize if height is smallest side
+        hpercent = (base / float(png.size[1]))
+        wsize = int((float(png.size[0]) * float(hpercent)))
+        png = png.resize((wsize, base), Image.Resampling.LANCZOS)
+    elif width < height:
+        ##Resize if width is smallest side
+        wpercent = (base / float(png.size[0]))
+        hsize = int((float(png.size[1]) * float(wpercent)))
+        png = png.resize((base, hsize), Image.Resampling.LANCZOS)
+
     ## Crop from center
     width, height = png.size
-    left = (width - baseHeight)/2
-    top = (height - baseHeight)/2
-    right = (width + baseHeight)/2
-    bottom = (height + baseHeight)/2
+    left = (width - base)/2
+    top = (height - base)/2
+    right = (width + base)/2
+    bottom = (height + base)/2
+
     png = png.crop((left, top, right, bottom))
     # print each
     if png.mode == 'RGBA':
